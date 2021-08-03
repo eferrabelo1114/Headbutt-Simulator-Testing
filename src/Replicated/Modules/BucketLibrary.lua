04cf28fff5536614e64b2f09f1081f4f61d55716
Ethan Ferrabelo <ethanmferrabelo@gmail.com>
2021-08-03 11:26:13 -0400

    Added Bucket Data

--- src/Replicated/Modules/BucketLibrary.lua
+++ src/Replicated/Modules/BucketLibrary.lua
@@ -3,88 +3,88 @@ local MainHammerData = {
     ["Default"] = {
         Texture = 6412632993;
         Price = 0;
-        HeadmuscleStorage = 1000;
+        HeadmuscleStorage = 20;
     };
     ["Dirt"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 35;
+        HeadmuscleStorage = 45;
     };
     ["Iron"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 160;
+        HeadmuscleStorage = 100;
     };
     ["Gold"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 500;
+        HeadmuscleStorage = 190;
     };
     ["Diamond"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 1350;
+        HeadmuscleStorage = 250;
     };
     ["Emerald"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 3600;
+        HeadmuscleStorage = 380;
     };
     ["Pineapple"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 9000;
+        HeadmuscleStorage = 500;
     };
     ["Cow"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 18500;
+        HeadmuscleStorage = 650;
     };
     ["Flame"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 45000;
+        HeadmuscleStorage = 900;
     };
     ["Camo"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 76000;
+        HeadmuscleStorage = 1350;
     };
     ["Mushroom"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 100000;
+        HeadmuscleStorage = 1600;
     };
     ["Pinkdiamond"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 235000;
+        HeadmuscleStorage = 2400;
     };
     ["Cloud"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 375000;
+        HeadmuscleStorage = 3300;
     };
     ["Industrial"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 850000;
+        HeadmuscleStorage = 5000;
     };
     ["IceFlame"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 1000000;
+        HeadmuscleStorage = 7500;
     };
     ["Lava"] = {
         Texture = 6412632993;
-        Price = 0;
-        HeadmuscleStorage = 1000;
+        Price = 2000000;
+        HeadmuscleStorage = 10000;
     };
 
     --Gamepass Hammers
     ["Rainbow"] = {
         Texture = 6412632993;
-        Price = 0;
+        Price = 400;
         HeadmuscleStorage = 1000;
     };
 }
